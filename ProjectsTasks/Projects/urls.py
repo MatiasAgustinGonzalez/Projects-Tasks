97fb3bfa1a10292a5579cc708a675f860de3fdf9
Matias Gonzalez <matigonzalez93@hotmail.com.ar>
2024-04-27 15:11:17 -0300

    V 1.2.1 | Agregado Vistas Varias

--- ProjectsTasks/Projects/urls.py
+++ ProjectsTasks/Projects/urls.py
@@ -2,15 +2,23 @@ from django.urls import path
 from .views import(
     home_view,
     TarjetaListView,
-    TarjetaCreateView,
     FilterBySponsorListView,
     FilterByLiderListView,
+    TarjetaCreateView,
+    LiderCreateView,
+    SponsorCreateView,
+    TarjetaDetailView,
+    TarjetaDeleteView,
 )
 
 urlpatterns = [
     path("", home_view, name="home"),
     path("cards-list/", TarjetaListView.as_view(), name="list_cards"),
-    path("cards-create/", TarjetaCreateView.as_view(), name="create_cards"),
     path("cards-filter-bysponsor/<str:sponsor_name>/", FilterBySponsorListView.as_view(), name="filter_sponsor"),
     path("cards-filter-bylider/<str:lider_name>/", FilterByLiderListView.as_view(), name="filter_lider"),
+    path("cards-create/", TarjetaCreateView.as_view(), name="create_cards"),
+    path("lider-create/", LiderCreateView.as_view(), name="create_lider"),
+    path("sponsor-create/",SponsorCreateView.as_view(), name="create_sponsor"),
+    path("cards-detail/<int:pk>/", TarjetaDetailView.as_view(), name="detail_cards"),
+    path("cards-delete/<int:pk>/", TarjetaDeleteView.as_view(), name="delete_cards"),
 ]
--- ProjectsTasks/Projects/views.py
+++ ProjectsTasks/Projects/views.py
@@ -18,16 +18,14 @@ def home_view(request):
 # --------------------------------   CRUD   -----------------------------------
 # -----------------------------------------------------------------------------
 
+#--------------------------------
+#   LISTAR todos los registros
+#--------------------------------
+
 class TarjetaListView(ListView):
     model = Tarjeta
     template_name = "Projects/cards/list_cards.html"
     context_object_name = "TARJETAS_LIST"
-
-class TarjetaCreateView(CreateView):
-    model = Tarjeta
-    template_name = "Projects/cards/form_create_cards.html"
-    fields = ['lider', 'sponsor', 'titulo', 'descripcion', 'fecha_inicio', 'fecha_fin','estado']
-    success_url = reverse_lazy("list_cards")
 
 class FilterBySponsorListView(ListView):
     model = Tarjeta
@@ -38,6 +36,12 @@ class FilterBySponsorListView(ListView):
         sponsor_name = self.kwargs['sponsor_name']
         return Tarjeta.objects.filter(sponsor__nombre=sponsor_name)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sponsor_name = self.kwargs['sponsor_name']
+        context['filtro_tipo'] = f'Sponsor ({sponsor_name})'
+        return context
+
 class FilterByLiderListView(ListView):
     model = Tarjeta
     template_name = "Projects/cards/filter_cards.html"
@@ -46,3 +50,59 @@ class FilterByLiderListView(ListView):
     def get_queryset(self):
         lider_name = self.kwargs['lider_name']
         return Tarjeta.objects.filter(lider__nombre=lider_name)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lider_name = self.kwargs['lider_name']
+        context['filtro_tipo'] = f'Líder ({lider_name})'
+        return context
+
+
+#--------------------------------
+#   CREAR todos los registros
+#--------------------------------
+
+class TarjetaCreateView(CreateView):
+    model = Tarjeta
+    template_name = "Projects/cards/form_create_cards.html"
+    fields = ['lider', 'sponsor', 'titulo', 'descripcion', 'fecha_inicio', 'fecha_fin','estado']
+    success_url = reverse_lazy("list_cards")
+
+class LiderCreateView(CreateView):
+    model = Lider
+    template_name = "Projects/cards/form_create_lider.html"
+    fields = ['nombre']
+    success_url = reverse_lazy("list_cards")
+
+class SponsorCreateView(CreateView):
+    model = Sponsor
+    template_name = "Projects/cards/form_create_sponsor.html"
+    fields = ['nombre']
+    success_url = reverse_lazy("list_cards")
+
+
+#--------------------------------
+#   LEER todos los registros
+#--------------------------------
+
+class TarjetaDetailView(DetailView):
+    model = Tarjeta
+    template_name = "Projects/cards/detail_cards.html"
+    context_object_name = "TARJETAS"
+
+
+#--------------------------------
+# ACTUALIZAR todos los registros
+#--------------------------------
+
+
+
+#--------------------------------
+#   BORRAR todos los registros
+#--------------------------------
+
+class TarjetaDeleteView(DeleteView):
+    model = Tarjeta
+    template_name = "Projects/cards/form_delete_cards.html"
+    success_url = reverse_lazy("list_cards")
+
