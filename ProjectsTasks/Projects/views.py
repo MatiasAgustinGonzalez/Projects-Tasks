70c92d2575cd4d84206465f8310ff1569b25eb9b
Matias Gonzalez <matigonzalez93@hotmail.com.ar>
2024-04-30 21:42:01 -0300

    V 1.3.1 | Agregado de Vistas de Edicion

--- ProjectsTasks/Projects/views.py
+++ ProjectsTasks/Projects/views.py
@@ -57,6 +57,15 @@ class FilterByLiderListView(ListView):
         context['filtro_tipo'] = f'Líder ({lider_name})'
         return context
 
+class LiderListView(ListView):
+    model = Lider
+    template_name = "Projects/master/list_detail_lider.html"
+    context_object_name = "LIST_DETAIL_LIDER"
+
+class SponsorListView(ListView):
+    model = Sponsor
+    template_name = "Projects/master/list_detail_sponsor.html"
+    context_object_name = "LIST_DETAIL_SPONSOR"
 
 #--------------------------------
 #   CREAR todos los registros
@@ -70,13 +79,13 @@ class TarjetaCreateView(CreateView):
 
 class LiderCreateView(CreateView):
     model = Lider
-    template_name = "Projects/cards/form_create_lider.html"
+    template_name = "Projects/master/form_create_lider.html"
     fields = ['nombre']
-    success_url = reverse_lazy("list_cards")
+    success_url = reverse_lazy("list_lider")
 
 class SponsorCreateView(CreateView):
     model = Sponsor
-    template_name = "Projects/cards/form_create_sponsor.html"
+    template_name = "Projects/master/form_create_sponsor.html"
     fields = ['nombre']
     success_url = reverse_lazy("list_cards")
 
@@ -95,6 +104,26 @@ class TarjetaDetailView(DetailView):
 # ACTUALIZAR todos los registros
 #--------------------------------
 
+class TarjetaUpdateView(UpdateView):
+    model = Tarjeta
+    template_name = "Projects/cards/edit_cards.html"
+    context_object_name = "TARJETAS"
+    fields = ["lider", "sponsor", "titulo", "descripcion", "fecha_inicio", "fecha_fin", "estado"]
+    success_url = reverse_lazy("list_cards")
+
+class LiderUpdateView(UpdateView):
+    model = Lider
+    template_name = "Projects/master/edit_lider.html"
+    context_object_name = "LIDER"
+    fields = ["nombre"]
+    success_url = reverse_lazy("list_lider")
+
+class SponsorUpdateView(UpdateView):
+    model = Sponsor
+    template_name = "Projects/master/edit_sponsor.html"
+    context_object_name = "SPONSOR"
+    fields = ["nombre"]
+    success_url = reverse_lazy("list_sponsor")
 
 
 #--------------------------------
@@ -104,5 +133,19 @@ class TarjetaDetailView(DetailView):
 class TarjetaDeleteView(DeleteView):
     model = Tarjeta
     template_name = "Projects/cards/form_delete_cards.html"
+    context_object_name = "TARJETAS"
     success_url = reverse_lazy("list_cards")
+
+class LiderDeleteView(DeleteView):
+    model = Lider
+    template_name = "Projects/master/form_delete_lider.html"
+    context_object_name = "LIDER"
+    success_url = reverse_lazy("list_lider")
+
+class SponsorDeleteView(DeleteView):
+    model = Sponsor
+    template_name = "Projects/master/form_delete_sponsor.html"
+    context_object_name = "SPONSOR"
+    success_url = reverse_lazy("list_sponsor")
+
 
